7038c0867d0fb83f7408a8d17be9cdaaaac19639
Oragbakosi Anthony Arinze <38960714+sypha999@users.noreply.github.com>
2022-06-18 21:17:04 +0100

    Update rockpaper.py

--- rockpaper.py
+++ rockpaper.py
@@ -7,36 +7,36 @@ def rps():
     cpu_choice = random.choice(possible_options)
     # starting a loop to get user input
     while True:
-        user_choice = input('\nChoose \'R\' for rock \'P\' for paper and \'S\' for scissors\n').upper()
+        user_choice = input('Choose \'R\' for rock \'P\' for paper and \'S\' for scissors\n').upper()
         if (user_choice != "R") and (user_choice != 'P') and (user_choice != 'S'):
-            print('\nInvalid Input Comrade\n')
+            print('Invalid Input Comrade\n')
         else:
 
             break
     if cpu_choice == user_choice:
-        print('\nIt\'s a tie, restarting game\n')
+        print('It\'s a tie, restarting game\n')
         rps()
     elif cpu_choice == 'R' and user_choice == 'P':
-        print('\nPlayer(Paper):CPU(Rock)\n')
-        print('\nPaper beats Rock\n')
-        print('\nPlayer wins\n')
+        print('Player(Paper):CPU(Rock)\n')
+        print('Paper beats Rock\n')
+        print('Player wins\n')
     elif cpu_choice == 'P' and user_choice == 'R':
-        print('\nPlayer(Rock):CPU(Paper)\n')
-        print('\nPaper beats Rock\n')
-        print('\nCPU wins\n')
+        print('Player(Rock):CPU(Paper)\n')
+        print('Paper beats Rock\n')
+        print('CPU wins\n')
     elif cpu_choice == 'R' and user_choice == 'S':
-        print('\nPlayer(Scissors):CPU(Rock)\n')
-        print('\nRock beats Scissors\n')
-        print('\nCPU wins\n')
+        print('Player(Scissors):CPU(Rock)\n')
+        print('Rock beats Scissors\n')
+        print('CPU wins\n')
     elif cpu_choice == 'S' and user_choice == 'R':
-        print('\nPlayer(Rock):CPU(Scissors)\n')
-        print('\nRock beats Scissors\n')
-        print('\nPlayer wins\n')
+        print('Player(Rock):CPU(Scissors)\n')
+        print('Rock beats Scissors\n')
+        print('Player wins\n')
     elif cpu_choice == 'P' and user_choice == 'S':
-        print('\nPlayer(Scissors):CPU(Paper)\n')
-        print('\nScissors beats Paper\n')
-        print('\nPlayer wins\n')
+        print('Player(Scissors):CPU(Paper)\n')
+        print('Scissors beats Paper\n')
+        print('Player wins\n')
     elif cpu_choice == 'S' and user_choice == 'P':
-        print('\nPlayer(Paper):CPU(Scissors)\n')
-        print('\nScissors beats Paper\n')
-        print('\nCPU wins\n')
+        print('Player(Paper):CPU(Scissors)\n')
+        print('Scissors beats Paper\n')
+        print('CPU wins\n')
